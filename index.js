8b0c8cc6a384a2a1468fbc448148f46fd6bd2f8c
ashraful2430 <www.savage582@gmail.com>
2024-01-24 02:47:50 +0600

    created api to see the single house info

--- index.js
+++ index.js
@@ -32,6 +32,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/rented/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await rentedHouseCollection.findOne(query);
+      res.send(result);
+    });
+
     app.patch("/rented/:id", async (req, res) => {
       const item = req.body;
       const id = req.params.id;
