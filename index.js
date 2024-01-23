bbbde83ea7dcb8f1334e30868e638fe0de5b80e3
ashraful2430 <www.savage582@gmail.com>
2024-01-24 03:53:51 +0600

    created delete api to delete the booked houses

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
       res.send({ count });
     });
 
+    app.delete("/booked/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookedHouseCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.post("/booked", async (req, res) => {
       const info = req.body;
       const result = await bookedHouseCollection.insertOne(info);
